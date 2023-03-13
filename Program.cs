c37b573f99030ad50755d5489b35b567a3139914
Andrea <102468525+andrea-zecevic@users.noreply.github.com>
2023-03-13 16:08:28 +0100

    Update Program.cs
    dodala

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace PISVjezba1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world, stan LOONA");
+            Console.WriteLine("Hello kolegice :) "); //komentar
         }
     }
 }
